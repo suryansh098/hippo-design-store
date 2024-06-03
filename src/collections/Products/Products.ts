3180d9d12ef2b39f2f279f5240cd7bebd5e3ece9
suryansh098 <suryanshyadav160@gmail.com>
2024-06-03 18:10:56 +0530

    added data models

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,116 @@
+import { PRODUCT_CATEGORIES } from "../../config";
+import { CollectionConfig } from "payload/types";
+
+export const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {},
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      label: "Product details",
+      type: "textarea",
+    },
+    {
+      name: "price",
+      label: "Price in USD",
+      min: 0,
+      max: 1000,
+      type: "number",
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: "productFiles",
+      label: "Product file(s)",
+      type: "relationship",
+      relationTo: "product_files",
+      required: true,
+      hasMany: false,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product status",
+      type: "select",
+      defaultValue: "pending",
+      options: [
+        { label: "Pending verification", value: "pending" },
+        { label: "Approved", value: "approved" },
+        { label: "Denied", value: "denied" },
+      ],
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+    },
+    {
+      name: "priceId",
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+    },
+    {
+      name: "stripeId",
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+    },
+    {
+      name: "images",
+      label: "Product image(s)",
+      type: "array",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,12 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import { buildConfig } from "payload/config";
-import { Users } from "./collections/users";
 import dotenv from "dotenv";
+import { Products } from "./collections/Products/Products";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFiles";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
