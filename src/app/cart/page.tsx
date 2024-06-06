66be283c0bfe8195702b268e994e806bde48da62
suryansh098 <suryanshyadav160@gmail.com>
2024-06-06 16:06:46 +0530

    added stripe payment

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,15 +4,28 @@ import { Button } from "@/components/ui/button";
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useCart } from "@/hooks/use-cart";
 import { cn, formatPrice } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
 import { Check, Loader2, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
   const { items, removeItem } = useCart();
+
+  const router = useRouter();
+
+  const { mutate: createCheckoutSession, isLoading } =
+    trpc.payment.createSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) router.push(url);
+      },
+    });
+
+  const productIds = items.map(({ product }) => product.id);
 
   useEffect(() => {
     setIsMounted(true);
@@ -185,7 +198,15 @@ const CartPage = () => {
             </div>
 
             <div className="mt-6">
-              <Button className="w-full" size="lg">
+              <Button
+                className="w-full"
+                size="lg"
+                disabled={isCartEmpty || isLoading}
+                onClick={() => createCheckoutSession({ productIds })}
+              >
+                {isLoading ? (
+                  <Loader2 className="w-4 h-4 animate-spin mr-1.5" />
+                ) : null}
                 Checkout
               </Button>
             </div>
--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -7,7 +7,7 @@ const isAdminOrHasAccessToImages =
     const user = req.user as User | null;
 
     if (!user) return false;
-    if (user.role === "admin") return false;
+    if (user.role === "admin") return true;
 
     return {
       user: {
